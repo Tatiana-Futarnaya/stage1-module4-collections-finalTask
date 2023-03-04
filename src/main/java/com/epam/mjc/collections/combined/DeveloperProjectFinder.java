54d949af5df44b46fcfdeaffc42507a4ab86f777
Tatiana-Futarnaya <tannia93@mail.ru>
2023-03-04 17:04:14 +0300

    first commit

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,31 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list=new ArrayList<>();
+        for (Map.Entry<String,Set<String>> item: projects.entrySet()) {
+            for (String value: item.getValue()) {
+                if(developer.equals(value)){
+                    list.add(item.getKey());
+                }
+            }
+        }
+        Collections.sort(list,new ListComparator());
+        return list;
+    }
+}
+
+class ListComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        int result=0;
+        if(o1.length()<o2.length()){
+            result=1;
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,33 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer,Set<String>> map=new HashMap<>();
+        Set<String> set=new HashSet<>();
+
+        for (Map.Entry<String,Integer> item: sourceMap.entrySet()) {
+            int x = item.getKey().length();
+            set.add(item.getKey());
+            map.put(x, null);
+
+
+
+            for (Map.Entry<Integer, Set<String>> value : map.entrySet()) {
+                value.setValue(new HashSet<>());
+                for (String i: set) {
+                    if(value.getKey()==i.length()){
+                        value.getValue().add(i);
+                    }
+                }
+
+
+            }
+        }
+        return map;
     }
 }
